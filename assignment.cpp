1902cc722f25fa95bfcc6a9502fa6e55fd3c05bc
vikasyadav5120 <67861862+vikasyadav5120@users.noreply.github.com>
2020-07-05 20:47:05 +0530

    Add files via upload

---
+++ assignment.cpp
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+
+int main()
+{
+int num1, num2, sum;
+printf("enter first no:");
+scanf("%d",&num1);
+printf("enter second no :");
+scanf("%d",&num2);
+
+sum = num1 + num2;
+printf("sum of the entered no is: %d" , sum);
+return 0;
+}
